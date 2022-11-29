2e90a61381d74e82b3ed2c5fc5f6f6cbcf4959a3
Jaakko <jiisala@gmail.com>
2022-11-29 12:29:17 +0200

    schema.sql added

---
+++ schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS entries CASCADE;
+DROP TABLE IF EXISTS tags CASCADE;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE,
+    password TEXT,
+);
+
+CREATE TABLE entries (
+    id SERIAL PRIMARY KEY,
+    author TEXT,
+    title TEXT,
+    publisher TEXT,
+    year INTEGER,
+    doi TEXT,
+    isbn TEXT,
+    editor TEXT,
+    pages TEXT,
+    shorthand TEXT,
+    user_id INTEGER,
+
+    user_id INTEGER REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    entry_id INTEGER REFERENCES etries ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    tag TEXT
+);
